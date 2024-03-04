c9f195f83bf50d9ce924a49d8d58487ffd2ed50b
Julia Chuprova <nikaniks57@gmail.com>
2024-03-04 18:43:22 +0200

    minor changes

--- practice/recursion.py
+++ practice/recursion.py
@@ -1,18 +1,19 @@
 # Define a function named list_sum that takes a list of numbers as input
-def list_sum(num_List):
+def list_sum(num_list):
 
     # Check if the length of the input list is 1
-    if len(num_List) == 1:
+    if len(num_list) == 1:
         # If the list has only one element, return that element
-        return num_List[0]
+        return num_list[0]
 
     else:
         # If the list has more than one element, return the sum of the first element
         # and the result of recursively calling the list_sum function on the rest of the list
-        return num_List[0] + list_sum(num_List[1:])
+        return num_list[0] + list_sum(num_list[1:])
 
 # Print the result of calling the list_sum function with the input [2, 4, 5, 6, 7]
 print(list_sum([2, 4, 5, 6, 7]))
+
 
 
 # Define a function named recursive_list_sum that calculates the sum of elements in a nested list
@@ -23,16 +24,16 @@ def recursive_list_sum(data_list):
     # Iterate through each element in the input list
     for element in data_list:
         # Check if the current element is a list (nested list)
-        if type(element) == type([]):
+        if type(element) == type([]):  # isinstance(element, List)
             # If the element is a list, recursively call the recursive_list_sum function on the element
-            total = total + recursive_list_sum(element)
+            total += recursive_list_sum(element)
         else:
             # If the element is not a list, add its value to the total
-            total = total + element
+            total += element
 
     # Return the total sum
     return total
 
 
 # Print the result of calling the recursive_list_sum function with the input list [1, 2, [3,4], [5,6]]
-print(recursive_list_sum([1, 2, [3, 4], [5, 6]]))
+print(recursive_list_sum([1, 2, [3, 4], [5, 6, [7, 8, 9]]]))
